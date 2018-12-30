784dddbb39ddd95d08176c519aaa288053db1361
ronen <narkisr@gmail.com>
2018-12-30 11:35:39 +1100

    latest re-scan

--- src/re_gent/zero/functions.clj
+++ src/re_gent/zero/functions.clj
@@ -1,7 +1,7 @@
 (ns re-gent.zero.functions
   (:require
    [re-share.oshi :refer (read-metrics os get-processes)]
-   [re-scan.core :refer [nmap open-ports]]
+   [re-scan.core :refer [nmap into-ports into-hosts]]
    [clojure.java.shell :refer [sh]]
    [cheshire.core :refer :all]
    [serializable.fn :refer :all]
