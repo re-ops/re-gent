b6209ebaf610d43e4fdf3a04bde4a2f9c6f5dd28
ronen <narkisr.com>
2019-06-21 20:15:54 +1000

    safe freeze

--- src/re_gent/zero/reply.clj
+++ src/re_gent/zero/reply.clj
@@ -7,13 +7,21 @@
 
 (def send-queue (atom (clojure.lang.PersistentQueue/EMPTY)))
 
+(defn safe-freeze
+  "Trying to freeze m failing without killing the agent"
+  [m]
+  (try (freeze m)
+       (catch Exception e
+         (error e))))
+
 (defn peek-send [socket]
   (when-let [m (peek @send-queue)]
-    (trace "sending" m)
-    (assert (= (.send socket (freeze m) 0) true))
-    (swap! send-queue pop)
-    (debug "sent" m)
-    true))
+    (when-let [ice (safe-freeze m)]
+      (debug "sending" m)
+      (assert (= (.send socket ice 0) true))
+      (swap! send-queue pop)
+      (debug "sent" m)
+      true)))
 
 (defn send- [m]
   (swap! send-queue conj m))
