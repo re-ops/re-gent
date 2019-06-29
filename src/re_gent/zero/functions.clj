0f3acb689b43cbb1590db47aec50a5d190c06a64
ronen <narkisr@gmail.com>
2019-06-29 23:29:44 +1000

    using latest re-cog ns

--- src/re_gent/zero/functions.clj
+++ src/re_gent/zero/functions.clj
@@ -1,13 +1,15 @@
 (ns re-gent.zero.functions
   (:require
    [re-share.oshi :refer (read-metrics os get-processes)]
-   [re-scan.core :refer [nmap into-ports into-hosts]]
+   [re-scan.core :refer (nmap into-ports into-hosts)]
    [clojure.java.shell :refer [sh]]
    [cheshire.core :refer :all]
    [serializable.fn :refer :all]
-   [re-cog.common :refer [require-constants require-functions bind-bash]]))
+   [re-cog.common.functions :refer (require-functions require-resources)]
+   [re-cog.common :refer (require-constants bind-bash)]))
 
 (require-functions)
+(require-resources)
 (require-constants)
 (bind-bash)
 
